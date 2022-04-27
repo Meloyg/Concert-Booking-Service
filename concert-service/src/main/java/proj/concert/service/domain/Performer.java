e502ccc34e8ac1e2ee97cf4d4211aab63cd94432
Nancy <88317853+nancy111573@users.noreply.github.com>
2022-04-27 19:11:03 +1200

    Feature/performer domain model (#10)
    * Added pr template (#2)
    
    Co-authored-by: Melo Guan <melo.guan@orionhealth.com>
    
    * added Performer domain model
    
    * edited Performer annotation to fix bug when populating database

--- concert-service/src/main/java/proj/concert/service/domain/Performer.java
+++ concert-service/src/main/java/proj/concert/service/domain/Performer.java
@@ -1,4 +1,110 @@
 package proj.concert.service.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import proj.concert.common.types.Genre;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="PERFORMERS")
 public class Performer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @Column(name="IMAGE_NAME")
+    private String imageName;
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+    @Column(name="BLURB",columnDefinition="LONGTEXT") //allow blurb to store more text
+    private String blurb;
+
+    public Performer() {
+    }
+
+    public Performer(Long id, String name, String imageName, Genre genre, String blurb) {
+        this.id = id;
+        this.name = name;
+        this.imageName = imageName;
+        this.genre = genre;
+        this.blurb = blurb;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public void setBlurb(String blurb) {
+        this.blurb = blurb;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Performer other = (Performer) obj;
+
+        return new EqualsBuilder()
+                .append(id, other.id)
+                .append(name, other.name)
+                .append(imageName, other.imageName)
+                .append(genre, other.genre)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(name)
+                .append(imageName)
+                .append(genre)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Performer{" +
+                "id=" + id +
+                ", name=" + name +
+                ", imageName=" + imageName +
+                ", genre=" + genre +
+                '}';
+    }
 }
