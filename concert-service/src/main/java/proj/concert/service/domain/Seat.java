ddfa3126fc15aae7657c48d2f49ac12d5c611497
Melo Guan <62586556+ygua699@users.noreply.github.com>
2022-04-27 19:15:17 +1200

    Seat & User Domain (#9)
    * Finished Seat Domain
    
    * Finished User Domain
    
    * Remove table name for user
    
    * Changed user column name

--- concert-service/src/main/java/proj/concert/service/domain/Seat.java
+++ concert-service/src/main/java/proj/concert/service/domain/Seat.java
@@ -5,12 +5,95 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class Seat{
+@Entity
+@Table(name = "SEAT")
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String label;
+    private boolean isBooked;
+    private LocalDateTime date;
+    private BigDecimal price;
 
-    // TODO Implement this class.
+    @Version
+    private Long version;
 
-	public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal cost) {	
-	}	
-	
-	public Seat() {}	
+    public Seat() {}
+
+    public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal price) {
+        this.label = label;
+        this.isBooked = isBooked;
+        this.date = date;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seat seat = (Seat) o;
+        return isBooked == seat.isBooked &&
+                Objects.equals(id, seat.id) &&
+                Objects.equals(label, seat.label) &&
+                Objects.equals(date, seat.date) &&
+                Objects.equals(price, seat.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, label, isBooked, date, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "id=" + id +
+                ", label='" + label + '\'' +
+                ", isBooked=" + isBooked +
+                ", date=" + date +
+                ", price=" + price +
+                '}';
+    }
+
 }
--- concert-service/src/main/java/proj/concert/service/domain/User.java
+++ concert-service/src/main/java/proj/concert/service/domain/User.java
@@ -1,4 +1,88 @@
 package proj.concert.service.domain;
 
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "VERSION")
+    @Version
+    private Long version;
+
+    @Column(name = "COOKIE")
+    private String cookie;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setUsername(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.username = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", version=" + version +
+                ", cookie='" + cookie + '\'' +
+                '}';
+    }
 }
